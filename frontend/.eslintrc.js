76a6aaaf128ff760070364ad099791203f81c729
2EETY <lty9251@naver.com>
2023-11-06 22:50:42 +0900

    feat, fix: 회원가입 기능 axios 구현, UI 변경

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     'linebreak-style': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.js'] }],
     'react/function-component-definition': 'off',
+    indent: 'off',
   },
 };
