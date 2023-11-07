eb1ff958b1c693280997d084c06e7b0405f8e3d8
euna0104 <koeuna0104@naver.com>
2023-11-07 17:14:15 +0900

    feat: 목표 삭제기능 구현

--- frontend/src/page/MyPage.jsx
+++ frontend/src/page/MyPage.jsx
@@ -6,17 +6,29 @@ import Header from '../component/Header';
 
 function MyPage() {
   const [editAccount, setEditAccount] = useState(false);
+  const [goals, setGoals] = useState([
+    { id: 1, name: '정보처리기사실기', date: '23/10/15~23/12/15' },
+    { id: 2, name: '정보처리기사필기', date: '23/10/15~23/12/15' },
+  ]); // 후에 백엔드에서 데이터 받아오도록 하기
 
   const editHandler = () => {
     setEditAccount(true);
   };
+
+  const onRemove = (id) => {
+    if (window.confirm('삭제하시겠습니까?')) {
+      const updatedGoals = goals.filter((goal) => goal.id !== id);
+      setGoals(updatedGoals);
+      console.log(`${id}번 게시물 삭제 완료`);
+    }
+  };
+
     return (
       <div className={styles.viewport}>
         <SideBar currentPage={6} />
         <div className={styles.mainprofile}>
           <Header />
           <div className={styles.headprofile}>
-            {/* 정보 가져오기 */}
             <div className={styles.userinfo}>
               <p className={styles.text}>홍길동</p>
               <p className={styles.mail}>gdflflk@gmail.com</p>
@@ -28,23 +40,16 @@ function MyPage() {
             {' '}
             <br />
             <div className={styles.goalwrap}>
-              {/* 정보 가져오기 */}
-              <div className={styles.goalbox}>
-                <p>정보처리기사실기</p>
-                <p>23/10/15~23/12/15</p>
-                <div className={styles.btnwrap}>
-                  <button type="button" className={styles.deletebtn}>Delete</button>
-                  <button type="button" className={styles.goaleditbtn}>Edit</button>
+              {goals.map((goal) => (
+                <div key={goal.id} className={styles.goalbox}>
+                  <p>{goal.name}</p>
+                  <p>{goal.date}</p>
+                  <div className={styles.btnwrap}>
+                    <button type="button" className={styles.deletebtn} onClick={() => onRemove(goal.id)}>Delete</button>
+                    <button type="button" className={styles.goaleditbtn}>Edit</button>
+                  </div>
                 </div>
-              </div>
-              <div className={styles.goalbox}>
-                <p>정보처리기사실기</p>
-                <p>23/10/15~23/12/15</p>
-                <div className={styles.btnwrap}>
-                  <button type="button" className={styles.deletebtn}>Delete</button>
-                  <button type="button" className={styles.goaleditbtn}>Edit</button>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
