692e55141119f46187f38d1ed174682fde0a0ae9
euna0104 <koeuna0104@naver.com>
2023-11-07 18:23:11 +0900

    feat: 로그아웃 기능 구현

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../style/Header.module.css';
 
 function Header() {
-    return (
-      <div className={styles.miniinfo}>
-        <div className={styles.headwrap}>
-          <p className={styles.nickname}>닉네임</p>
-          <p className={styles.logout}>Logout</p>
-        </div>
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    if (window.confirm('로그아웃 하시겠습니까?')) {
+      sessionStorage.removeItem('userId'); // 임시 변수
+      navigate('/api/login');
+      console.log('로그아웃 되었습니다.');
+    }
+  };
+  return (
+    <div className={styles.miniinfo}>
+      <div className={styles.headwrap}>
+        <p className={styles.nickname}>닉네임</p>
+        <button type="button" className={styles.logout} onClick={handleLogout}>로그아웃</button>
       </div>
-    );
+    </div>
+  );
 }
 export default Header;
