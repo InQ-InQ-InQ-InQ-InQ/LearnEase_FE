2483689713caeb162fc8a8d750801874c54fa76a
euna0104 <koeuna0104@naver.com>
2023-11-07 21:48:41 +0900

    feat: 로그아웃, 헤더에 사용자정보 기능

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import styles from '../style/Header.module.css';
 
 function Header() {
+  const [userName, setUserName] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get('/api/users/profile')
+    .then((response) => {
+      const user = response.data;
+      setUserName(user.userName);
+    })
+    .catch((error) => {
+      console.log('이름을 가져오지 못했습니다', error);
+    });
+  }, []);
 
   const handleLogout = () => {
     if (window.confirm('로그아웃 하시겠습니까?')) {
-      sessionStorage.removeItem('userId'); // 임시 변수
+      sessionStorage.removeItem('accessToken');
       navigate('/api/login');
       console.log('로그아웃 되었습니다.');
     }
@@ -15,7 +28,10 @@ function Header() {
   return (
     <div className={styles.miniinfo}>
       <div className={styles.headwrap}>
-        <p className={styles.nickname}>닉네임</p>
+        <p className={styles.nickname}>
+          홍길동
+          {userName}
+        </p>
         <button type="button" className={styles.logout} onClick={handleLogout}>로그아웃</button>
       </div>
     </div>
