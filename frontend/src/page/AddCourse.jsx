4de2ecaf6621f42e095390567a57345d6d3c7ca6
euna0104 <koeuna0104@naver.com>
2023-12-12 13:22:56 +0900

    feat: 도서,강의 챕터 추가 기능

--- frontend/src/page/AddCourse.jsx
+++ frontend/src/page/AddCourse.jsx
@@ -3,49 +3,64 @@ import PropTypes from 'prop-types';
 import styles from '../style/AddCourse.module.css';
 
 const AddCourse = ({ onClose, onAddCourse }) => {
-    const [courseName, setCourseName] = useState('');
-    const [chapters, setChapters] = useState([]);
+  const [courseName, setCourseName] = useState('');
+  const [chapters, setChapters] = useState([{ title: 'Chapter 1', subTitle: '' }]);
 
-    const saveCourseHandler = () => {
-        // 추가된 챕터를 chapters 배열에 추가
-        const newChapter = { title: `Chapter ${chapters.length + 1}`, subTitle: '' };
-        setChapters([...chapters, newChapter]);
-        // 사용자가 추가한 코스 정보 Make 컴포넌트로 전달
-        const courseData = { courseName, chapters };
-        onAddCourse(courseData);
-        onClose();
-    };
-    const outHandler = () => {
-        onClose();
-    };
-    return (
-      <div className={styles.screen}>
-        <div className={styles.addscreen}>
-          <button type="button" className={styles.out} onClick={outHandler}>X</button>
-          <div className={styles.box}>
-            <p className={styles.text}>도서 및 강의명</p>
-            <input type="text" className={styles.input1} placeholder="도서 및 강의명을 입력해주세요" onChange={(e) => setCourseName(e.target.value)} />
-            <div className={styles.innerbox}>
-              <p className={styles.text}>챕터</p>
-              <div className={styles.wrap}>
-                <p className={styles.text}>Chapter 1</p>
-                <input type="text" className={styles.input2} placeholder="소제목을 입력해주세요" />
-                <button type="button" className={styles.deletebtn}>-</button>
+  const addChapterHandler = () => {
+    const newChapter = { title: `Chapter ${chapters.length + 1}`, subTitle: '' };
+    setChapters([...chapters, newChapter]);
+  };
+
+  const saveCourseHandler = () => {
+    const courseData = { courseName, chapters };
+    onAddCourse(courseData);
+    onClose();
+  };
+
+  const outHandler = () => {
+    onClose();
+  };
+
+  return (
+    <div className={styles.screen}>
+      <div className={styles.addscreen}>
+        <button type="button" className={styles.out} onClick={outHandler}>X</button>
+        <div className={styles.box}>
+          <p className={styles.text}>도서 및 강의명</p>
+          <input
+            type="text"
+            className={styles.input1}
+            placeholder="도서 및 강의명을 입력해주세요"
+            onChange={(e) => setCourseName(e.target.value)}
+          />
+          <div className={styles.innerbox}>
+            {chapters.map((chapter, index) => (
+              <div key={`chapter-${index}`} className={styles.wrap}>
+                <p className={styles.text}>{chapter.title}</p>
+                <input
+                  type="text"
+                  className={styles.input2}
+                  placeholder="소제목을 입력해주세요"
+                  onChange={(e) => {
+                    const updatedChapters = [...chapters];
+                    updatedChapters[index].subTitle = e.target.value;
+                    setChapters(updatedChapters);
+                  }}
+                />
               </div>
-              <div className={styles.wrap}>
-                <p className={styles.text}>Chapter 1</p>
-                <input type="text" className={styles.input2} placeholder="소제목을 입력해주세요" />
-                <button type="button" className={styles.deletebtn}>-</button>
-              </div>
-            </div>
-            <button type="button" className={styles.submitbtn} onClick={saveCourseHandler}>챕터 추가하기</button>
+            ))}
+            <button type="button" className={styles.addChapterBtn} onClick={addChapterHandler}>+</button>
           </div>
+          <button type="button" className={styles.submitbtn} onClick={saveCourseHandler}>챕터 추가하기</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
+
 AddCourse.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    onAddCourse: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onAddCourse: PropTypes.func.isRequired,
 };
+
 export default AddCourse;
