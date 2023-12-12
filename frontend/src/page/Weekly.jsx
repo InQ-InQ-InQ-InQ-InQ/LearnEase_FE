9dc7895769c6e32a276cedec440efdc3d2944ba7
euna0104 <koeuna0104@naver.com>
2023-12-13 01:34:45 +0900

    refactor: data 리팩토링

--- frontend/src/page/Weekly.jsx
+++ frontend/src/page/Weekly.jsx
@@ -6,53 +6,58 @@ import DayInfo from '../component/DayInfo';
 import AddTask from './AddTask';
 
 const Weekly = () => {
-    const accessToken = sessionStorage.getItem('accessToken');
-    const [addTask, setAddTask] = useState(false); // task 추가
-    const [days, setDays] = useState([
-        { name: 'Mon', tasks: ['a', 'b'] },
-        { name: 'Tue', tasks: ['c', 'd'] },
-        { name: 'Wed', tasks: ['e', 'f'] },
-        { name: 'Thu', tasks: ['g', 'h'] },
-        { name: 'Fri', tasks: ['i', 'j'] },
-        { name: 'Sat', tasks: ['k', 'l'] },
-        { name: 'Sun', tasks: ['m', 'n'] },
-    ]);
+  const [addTask, setAddTask] = useState(false);
 
   const taskHandler = () => {
     setAddTask(true);
   };
-    return (
-      <div className={styles.viewport}>
-        <SideBar currentPage={2} />
-        <div className={styles.container}>
-          <Header />
-          <div className={styles.contents}>
-            <DayInfo />
-            <div className={styles.midwrap}>
-              <button type="button" className={styles.addbtn} onClick={taskHandler}>Add New Task</button>
-            </div>
-            <div className={styles.mainwrap}>
-              {days.map((day) => (
-                <div key={day.name} className={styles.day}>
-                  <div className={styles.dayhead}>{day.name}</div>
-                  <div className={styles.daycontents}>
-                    {day.tasks.map((task, taskIndex) => (
-                      <>
-                        <div className={styles.taskbox}>
-                          <input type="checkbox" />
-                          <div key={taskIndex}>{task}</div>
-                        </div>
-                        <br />
-                      </>
-                        ))}
-                  </div>
+
+  const closeAddTaskHandler = () => {
+    setAddTask(false);
+  };
+
+  const days = [
+    { name: 'Mon', tasks: ['4과목-서버프로그램 구현', '4과목-프로그래밍언어 활용'] },
+    { name: 'Tue', tasks: [] },
+    { name: 'Wed', tasks: ['4과목-응용SW 기초기술 활용', '5과목-소프트웨어 개발방법론 활용'] },
+    { name: 'Thu', tasks: [] },
+    { name: 'Fri', tasks: ['5과목-IT프로젝트 정보시스템 구축관리', '5과목-시스템 보안 구축'] },
+    { name: 'Sat', tasks: [] },
+    { name: 'Sun', tasks: [] },
+  ];
+
+  return (
+    <div className={styles.viewport}>
+      <SideBar currentPage={2} />
+      <div className={styles.container}>
+        <Header />
+        <div className={styles.contents}>
+          <DayInfo />
+          <div className={styles.midwrap}>
+            <button type="button" className={styles.addbtn} onClick={taskHandler}>
+              Add New Task
+            </button>
+          </div>
+          <div className={styles.mainwrap}>
+            {days.map((day) => (
+              <div key={day.name} className={styles.day}>
+                <div className={styles.dayhead}>{day.name}</div>
+                <div className={styles.daycontents}>
+                  {day.tasks.map((task, index) => (
+                    <div key={`${day.name}-${index}`} className={styles.taskbox}>
+                      <input type="checkbox" />
+                      <div>{task}</div>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
-        {addTask && <AddTask />}
       </div>
-    );
+      {addTask && <AddTask onClose={closeAddTaskHandler} />}
+    </div>
+  );
 };
+
 export default Weekly;
